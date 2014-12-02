736a157d37cc39193663e5998c546cac095a9ca1
Matt Jadud <matt@jadud.com>
2014-12-02 08:54:49 -0500

    Addes in ROM emission.

--- compiler/create-ids.rkt
+++ compiler/create-ids.rkt
@@ -138,5 +138,3 @@
    (goto 'ENDIF-LABEL110387)
    (label 'ENDIF-LABEL110387)
    (label 'ENDWHILE110384)))
-
-(define 
--- tinyasm/tinyasm.rkt
+++ tinyasm/tinyasm.rkt
@@ -5,30 +5,64 @@
 (require racket/cmdline)
 (require racket/string)
 
+(define output-rom (make-parameter false))
+
+(define (mangle-name infile)
+  (regexp-replace ".asm" infile ".hack"))
+
+(define (pad4 str)
+  (define zeroes (- 4 (string-length str)))
+  (string-append
+   (make-string zeroes #\0) 
+   str))
+
+(define (make-spaces str tot)
+  (define spaces (- tot (string-length str)))
+  (make-string spaces #\space))
+
 (define (process infile outfile)
   (define outp (open-output-file outfile #:exists 'replace))
   
   (define lines (file->list infile read-line))
   
+  (when (output-rom)
+    (fprintf outp "uint16_t ROM[] = {~n")
+    )
+  
   (for ([line lines]
         [lineno (range 1 (length lines))])
     (set! line (string-trim line " "))
+    
     (when (< 0 (string-length line))
+      
       (when (verbose-mode)
         (printf "Line ~a: ~a~n" lineno line))
+      (define converted (convert-instruction line))
+      (define hex (number->string (string->number converted 2) 16))
       
       (cond
         [(comment-mode)
          (fprintf outp "~a\t// ~a~n" 
-                  (convert-instruction line lineno)
+                  converted
                   line)]
+        [(output-rom)
+         (fprintf outp "\t0x~a, /* ~a ~a => ~a */~n" 
+                  (pad4 hex)
+                  line
+                  (make-spaces line 8)
+                  converted
+                  )]
+        
         [else
-         (fprintf outp "~a~n" (convert-instruction line lineno))])
+         (fprintf outp "~a~n" converted)])
                   
       
       (when (verbose-mode)
         (printf "~n"))
       ))
+  
+  (when (output-rom)
+    (fprintf outp "};~n"))
   
   (close-output-port outp)
   )
@@ -42,5 +76,18 @@
                        (verbose-mode true)]
    [("-c" "--comments") "Print comments on every line."
                         (comment-mode true)]
-   #:args (infile outfile)
-   (process infile outfile)))
+   [("-r" "--rom") "Output a rom.h file."
+                   (output-rom true)]
+   #:args (infile)
+   (begin
+     (unless (regexp-match ".asm" infile)
+       (printf "Input file must end with a .asm extension.~n")
+       (exit))
+     (define outfile (mangle-name infile))
+     
+     (cond
+       [(output-rom)
+        (process infile "rom.h")]
+       [else
+        (process infile outfile)])
+     )))
